70b21328ac454d47863df2384c434487a0cfae20
Blaine Bublitz <blaine.bublitz@gmail.com>
2016-06-10 21:27:56 -0500

    Update: Switch to mocha + expect for testing

--- test/default-resolution.js
+++ test/default-resolution.js
@@ -1,62 +1,61 @@
 'use strict';
 
-var lab = exports.lab = require('lab').script();
-var code = require('code');
+var expect = require('expect');
 
 var defaultResolution = require('../');
 var nodeVersion = require('../node-version');
 
-lab.describe('nodeVersion', function() {
+describe('nodeVersion', function() {
 
-  lab.it('has all integers and not strings', function(done) {
-    code.expect(nodeVersion.major).to.be.a.number();
-    code.expect(nodeVersion.minor).to.be.a.number();
-    code.expect(nodeVersion.patch).to.be.a.number();
+  it('has all integers and not strings', function(done) {
+    expect(nodeVersion.major).toBeA('number');
+    expect(nodeVersion.minor).toBeA('number');
+    expect(nodeVersion.patch).toBeA('number');
     done();
   });
 });
 
-lab.describe('defaultResolution', function() {
+describe('defaultResolution', function() {
   // Typically I don't unit test helpers, but this reduces the last run tests
   var major = nodeVersion.major;
   var minor = nodeVersion.minor;
 
-  lab.afterEach(function(done) {
+  afterEach(function(done) {
     nodeVersion.major = major;
     nodeVersion.minor = minor;
     done();
   });
 
-  lab.it('should return default resolution to 1000 (1 second) on node v0.10', function(done) {
+  it('should return default resolution to 1000 (1 second) on node v0.10', function(done) {
     nodeVersion.major = 0;
     nodeVersion.minor = 10;
-    code.expect(defaultResolution()).to.equal(1000);
+    expect(defaultResolution()).toEqual(1000);
     done();
   });
 
-  lab.it('should return default resolution to 1 (millisecond) on node v0.11', function(done) {
+  it('should return default resolution to 1 (millisecond) on node v0.11', function(done) {
     nodeVersion.major = 0;
     nodeVersion.minor = 11;
-    code.expect(defaultResolution()).to.equal(1);
+    expect(defaultResolution()).toEqual(1);
     done();
   });
 
-  lab.it('should return default resolution to 1 (millisecond) on node v0.12', function(done) {
+  it('should return default resolution to 1 (millisecond) on node v0.12', function(done) {
     nodeVersion.major = 0;
     nodeVersion.minor = 12;
-    code.expect(defaultResolution()).to.equal(1);
+    expect(defaultResolution()).toEqual(1);
     done();
   });
 
-  lab.it('should return default resolution to 1 (millisecond) on iojs v1.5', function(done) {
-    nodeVersion.major = 1;
-    nodeVersion.minor = 1;
-    code.expect(defaultResolution()).to.equal(1);
+  it('should return default resolution to 1 (millisecond) on node v4.3', function(done) {
+    nodeVersion.major = 4;
+    nodeVersion.minor = 3;
+    expect(defaultResolution()).toEqual(1);
     done();
   });
 
-  lab.it('should return default resolution passed as argument', function(done) {
-    code.expect(defaultResolution(2000)).to.equal(2000);
+  it('should return default resolution passed as argument', function(done) {
+    expect(defaultResolution(2000)).toEqual(2000);
     done();
   });
 
